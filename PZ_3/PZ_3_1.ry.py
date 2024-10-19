36b0e216b5c919be20235bc1531e6c1be1e19165
sivimim <ssivimim@gmail.com>
2024-10-19 16:26:31 +0300

    определила порядковый номер числа

--- PZ_3/PZ_3_1.ry.py
+++ PZ_3/PZ_3_1.ry.py
@@ -1,1 +1,5 @@
-print("hi")
+a, b, c = input("Введите первое число: "), input("Введите второе число :"), input("Введите третье число :")
+
+def is_m(a,b,c):
+  return a == b == c
+print(is_m(a, b, c))
