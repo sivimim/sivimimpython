a3874c2e3ed595c3d99c2aa176c18c7872a51f8b
sivimim <119958060+sivimim@users.noreply.github.com>
2024-12-14 16:30:08 +0300

    Update PZ_3_1.py

--- PZ_3/PZ_3_1.py
+++ PZ_3/PZ_3_1.py
@@ -1,5 +1,15 @@
-a, b, c = input("Введите первое число: "), input("Введите второе число :"), input("Введите третье число :")
+import random #генерация случайных чисел
 
-def is_m(a,b,c):
-  return a == b == c
-print(is_m(a, b, c))
+def Treugol(A,B,C):
+    return (A < B+C) and (B < A+C) and (C < A+B)
+
+a,b,c = [random.randrange(1,6) for i in range(0,3)] #random.randrange - возвращает случайное целое из диапазона
+while not Treugol(a,b,c):
+    a,b,c = [random.randrange(1,6) for i in range(0,3)]
+
+print("Длина a: ", a)
+print("Длина b: ", b)
+print("Длина c: ", c)
+
+prim = (a == b and a == c)
+print("Треугольник является равносторонним: ", prim)
